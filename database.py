d6969b87f58fa80b6eafb7e07ce0cc2ddbe13937
gabbaromero <romerosilviagabriela50@gmail.com>
2025-07-09 19:28:55 -0300

    Mejora estructura de base de datos

---
+++ database.py
@@ -0,0 +1,74 @@
+ALUMNOS = {
+    "caroliv": {
+        "nombre": "Carlos",
+        "apellido": "Olivera",
+        "edad": 25,
+        "contrasena": "ca1234",
+        "materias_aprobadas": ["Matemática", "Contabilidad", "Estadística I"],
+        "notas": [8, 9, 7]
+    },
+    "silvi56": {
+        "nombre": "Silvina",
+        "apellido": "Perez",
+        "edad": 28,
+        "contrasena": "silper",
+        "materias_aprobadas": ["Matemática", "Contabilidad", "Filosofía", "Álgebra I"],
+        "notas": [5, 6, 6, 8]
+    },
+    "brumar": {
+        "nombre": "Bruno", 
+        "apellido": "Martinez", 
+        "edad": 23, 
+        "contraseña": "bm8754",
+        "materias_aprobadas": ["Matemática", "Estadística I", "Estadística II", "Introducción a la Economía", "Microeconomía"],
+        "notas":[8, 9, 6, 10, 7]
+        },   
+    "san79": {
+        "nombre": "Santiago", 
+        "apellido": "Saldivar", 
+        "edad": 42, 
+        "contraseña": "sasa34",
+        "materias_aprobadas": ["Matemática", "Contabilidad", "Estadistica I", "Filosofía", "Estadística II", "Algebra I", "Estadística II","Microeconomía", "Macroeconomía"],
+        "notas": [10, 9, 9, 10, 8, 9, 9, 8, 10]
+        },
+    "grisua23":{
+        "nombre": "Grisel", 
+        "apellido": "Suarez", 
+        "edad": 23, 
+        "contraseña": "897gri",
+        "materias_aprobadas": ["Matemática", "Estadística I", "Introducción a la Economía", ],
+        "notas": [6, 10, 7]
+        },
+    "elifer": {
+        "nombre": "Elias", 
+        "apellido": "Fernandez", 
+        "edad": 28, 
+        "contraseña": "ferel31",
+        "materias_aprobadas": ["Matemática", "Microeconomía"],
+        "notas":[5, 8]
+        },
+    "javi67": {
+        "nombre": "Javier", 
+        "apellido": "Gonzalez", 
+        "edad": 31, 
+        "contraseña": "jg9806",
+        "materias_aprobadas": ["Matemática", "Estadística I", "Estadística II", "Filosofa", "Introducción a la Economía", "Microeconomía", "Derecho", "Administración"],
+        "notas":[7, 8, 10, 6, 7, 7, 6, 9]
+    },
+}
+
+# Materias disponibles
+MATERIAS_DISPONIBLES = {
+    "MAT": "Matemática",
+    "ALI": "Álgebra I",
+    "ESTII": "Estadística II",
+    "MIE": "Microeconomía",
+    "MAE": "Macroeconomía"
+}
+
+# Turnos de cursada
+TURNOS = {
+    "manana": "08:00 a 12:00",
+    "tarde": "13:00 a 17:00",
+    "noche": "18:00 a 22:00"
+}
