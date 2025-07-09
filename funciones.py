ea187b677c7b639089000347b81001dfc145c683
gabbaromero <romerosilviagabriela50@gmail.com>
2025-07-09 19:29:41 -0300

    Agrega función de notas con validaciones

---
+++ funciones.py
@@ -0,0 +1,30 @@
+import datetime
+
+def agregar_nota(usuario, materia, nota, alumnos_db):
+
+    # Validaciones
+    if not 1 <= nota <= 10:
+        raise ValueError("La nota debe estar entre 1 y 10")
+    if usuario not in alumnos_db:
+        raise KeyError(f"Usuario {usuario} no registrado")
+    if materia not in alumnos_db[usuario]["materias_aprobadas"]:
+        return False
+    
+    # Registro estructurado con fecha
+    registro = {
+        "materia": materia,
+        "nota": nota,
+        "fecha": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Formato: AAAA-MM-DD HH:MM:SS
+    }
+    
+    alumnos_db[usuario]["notas"].append(registro)
+    return True
+
+def obtener_alumno(usuario):
+    """Devuelve los datos de un alumno específico"""
+    return ALUMNOS.get(usuario)
+
+def verificar_contrasena(usuario, contrasena):
+    """Valida las credenciales de acceso"""
+    alumno = obtener_alumno(usuario)
+    return alumno and alumno["contrasena"] == contrasena
